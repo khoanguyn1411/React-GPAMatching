0959d51c9cc3428a86a6800bb7ef4b74ca17e39a
khoanguyn1411 <khoaah1411@gmail.com>
2023-01-21 01:43:40 +0700

    Compatize with firebaseui warning CLUB-438

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -4,7 +4,6 @@ import "firebase/compat/auth";
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
