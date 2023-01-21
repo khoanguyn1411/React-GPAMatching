44e97b16b8483d74360339c8f9817b53e27a0b93
khoanguyn1411 <khoaah1411@gmail.com>
2023-01-21 18:25:47 +0700

    Redeploy

--- src/main.tsx
+++ src/main.tsx
@@ -9,15 +9,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const queryClient = new QueryClient();
-console.log({
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <App />
