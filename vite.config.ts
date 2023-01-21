9875e419fd6b9f7dc8e626d8227c746f594e771c
khoanguyn1411 <khoaah1411@gmail.com>
2023-01-21 18:53:49 +0700

    Redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,16 @@ import checker from "vite-plugin-checker";
 import svgr from "vite-plugin-svgr";
 import { defineConfig } from "vite";
 
+const viteEnv = {}
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key]
+  }
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: viteEnv,
   plugins: [react(), viteTsconfigPaths(), checker({ typescript: true }), svgr()],
   server: { port: 5000, host: true },
   build: {
