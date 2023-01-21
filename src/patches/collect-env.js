2eeeb705663e30fcbd4fe388367b2ac46d5398af
khoanguyn1411 <khoaah1411@gmail.com>
2023-01-21 20:13:01 +0700

    Remove esling for js and jsx

--- src/patches/collect-env.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-const vite_env_variables = Object.entries(process.env).filter((variable) =>
-  variable[0].startsWith("VITE_"),
-);
-const vite_env_variable_string = vite_env_variables
-  .map((variable) => variable[0] + "=" + variable[1])
-  .join("\n");
-fs.writeFileSync(".env", vite_env_variable_string, { encoding: "utf-8" });
