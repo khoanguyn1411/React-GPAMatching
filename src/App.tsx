d2245f6114c6450382b5fc2fbba53adf99add378
khoanguyn1411 <khoaah1411@gmail.com>
2023-02-03 18:40:54 +0700

    Update code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { AppProvider } from "./providers";
 import { RootRoutes } from "./routes/RootRoutes";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      retryDelay: 4000,
+      refetchInterval: 30 * 1000,
+    },
+  },
+});
+
 const App: React.FC = () => {
   return (
-    <AppProvider>
-      <RootRoutes />
-    </AppProvider>
+    <QueryClientProvider client={queryClient}>
+      <AppProvider>
+        <RootRoutes />
+      </AppProvider>
+    </QueryClientProvider>
   );
 };
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,9 @@
 import "animate.css";
 import "./styles/fonts.css";
 import "./styles/global.css";
-import "./styles/variables.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      refetchOnWindowFocus: false,
-      retryDelay: 4000,
-      refetchInterval: 30 * 1000,
-    },
-  },
-});
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>,
-);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
